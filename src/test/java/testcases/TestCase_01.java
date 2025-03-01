74bfb3ad23e12f1e16573cec7850845844a4dd7d
kimi agarwal <kimi.agarwal@tothenew.com>
2025-03-02 00:13:21 +0530

    first commit

--- src/test/java/testcases/TestCase_01.java
+++ src/test/java/testcases/TestCase_01.java
@@ -13,12 +13,12 @@ public class TestCase_01{
 	  ChromeDriver driver = new ChromeDriver();
 	  driver.get("http://52.38.135.214/");
 	  driver.manage().window().maximize();
-	  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-	  LoginPage lp = new LoginPage(driver);
-	  lp.email("kimi.agarwal@tothenew.com");
-	  lp.password("12345678");
-	  lp.submit();
-	  System.out.println("Logged In successfully");
+		/*
+		 * driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS); LoginPage lp
+		 * = new LoginPage(driver); lp.email("kimi.agarwal@tothenew.com");
+		 * lp.password("12345678"); lp.submit();
+		 * System.out.println("Logged In successfully");
+		 */
 	  
   }
 }
